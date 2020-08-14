b4cbf651bbf21e214c920f9e4393fc44bce20421
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2020-08-13 23:24:31 -0300

    Add date param parsing

--- src/main/java/org/github/serverless/api/arguments/apigw/parsers/types/ParameterTypeAdjuster.java
+++ src/main/java/org/github/serverless/api/arguments/apigw/parsers/types/ParameterTypeAdjuster.java
@@ -1,20 +1,27 @@
 package org.github.serverless.api.arguments.apigw.parsers.types;
 
 import java.lang.reflect.Parameter;
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public abstract class ParameterTypeAdjuster {
 
     public Object adjust(Parameter parameter, String argument) {
 
         try {
-            if (Integer.class.equals(parameter.getType())) {
+            Type type = parameter.getType();
+            if (Integer.class.equals(type)) {
                 return Integer.parseInt(argument);
-            } else if (Boolean.class.equals(parameter.getType())) {
+            } else if (Boolean.class.equals(type)) {
                 return Boolean.parseBoolean(argument);
-            } else if (List.class.equals(parameter.getType())){
+            } else if (List.class.equals(type)){
                 return Arrays.asList(argument.split(","));
+            } else if (Date.class.equals(type)) {
+                return new SimpleDateFormat("dd-MMM-yyyy",  Locale.ENGLISH).parse(argument);
             }
             return argument;
         } catch (Exception e) {
